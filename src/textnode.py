96ca312a0f0127df4b0cb4ad4c1b6bcc1e2640f6
SkyfuryX <gannonshanley@gmail.com>
2025-12-28 15:04:10 -0500

    markdown spltting functions done

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from htmlnode import HTMLNode, ParentNode, LeafNode
+import re
 
 class TextType(Enum):
     TEXT = "text" # text (plain)
@@ -65,3 +66,61 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.extend(new)
     return new_nodes
         
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes: 
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        node_text = node.text
+        matches = extract_markdown_images(node_text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+        for image_alt, image_link in matches:
+            split_text = node_text.split(f"![{image_alt}]({image_link})", 1)
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], TextType.TEXT))    
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+            node_text = split_text[1] 
+        if node_text != "":
+            new_nodes.append(TextNode(node_text, TextType.TEXT))       
+    return new_nodes
+    
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes: 
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        node_text = node.text
+        matches = extract_markdown_links(node_text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+        for text, url in matches:
+            split_text = node_text.split(f"[{text}]({url})", 1)
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], TextType.TEXT))    
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+            node_text = split_text[1] 
+        if node_text != "":
+            new_nodes.append(TextNode(node_text, TextType.TEXT))       
+    return new_nodes
+
+def text_to_textnodes(text):
+    lines = text.splitlines()
+    nodes = []
+    for line in lines:
+        nodes.append(TextNode(line, TextType.TEXT))
+    nodes = split_nodes_delimiter(nodes, '**', TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, '_', TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, '`', TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    return split_nodes_link(nodes)
